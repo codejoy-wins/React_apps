098a80ab8f8d33f8551905482641f99dca6b2ced
Max <mjann@ucsc.edu>
2019-02-11 16:42:29 -0800

    first

--- src/App.js
+++ src/App.js
@@ -4,21 +4,38 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let num = 0;
+    const msg = "React Variable es6";
+    const myFunc = () =>{
+        console.log(`myfunc n is ${num}`);
+        num++;
+        document.getElementById('output').innerHTML = num;
+    }
+    const reset = () =>{
+      console.log(`resetting`);
+      num = 0
+      document.getElementById('output').innerHTML = num;
+  }
+    const myStyle = {
+      background: 'blue',
+      color: 'white',
+      padding: '12px'
+    }
+    const myStyle2 = {
+      background: 'green',
+      color: 'aqua',
+      padding: '8px'
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div id="output">
+            output spot
+          </div>
+          <h1>{msg}</h1>
+          <button style = {myStyle} onClick={myFunc}>Magic Button</button>
+          <button style = {myStyle2} onClick={reset}>Reset</button>
         </header>
       </div>
     );
