7c395940f8e1934e8c4beae6e0d6eb4c80f3f708
Max <mjann@ucsc.edu>
2019-02-13 13:04:24 -0800

    trying to add sound and failing

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -4,12 +4,24 @@ import Sound from 'react-sound';
 export class Sound extends Component {
   render() {
     return (
-      <Sound
-      url="mozart.mp3"
-      playStatus={Sound.status.PLAYING}
-      />
-    )
+        <Sound
+          url="mozart.mp3"
+          playStatus={Sound.status.PLAYING}
+          playFromPosition={300 /* in milliseconds */}
+          onLoading={this.handleSongLoading}
+          onPlaying={this.handleSongPlaying}
+          onFinishedPlaying={this.handleSongFinishedPlaying}
+        />
+    );
+    // return (
+    //   <Sound
+    //   url="mozart.mp3"
+    //   playStatus={Sound.status.PLAYING}
+    //   />
+    // )
   }
 }
 
 export default Sound
+
+// I want a component that I can show or hide and play music from
